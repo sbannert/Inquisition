c42371f1919061cde9b1a481ae2261438aab0103
geirgie25 <43558674+geirgie25@users.noreply.github.com>
2018-10-13 00:41:26 -0500

    Bug fixes for enemies
    ^

--- Assets/HomingEnemyScript.cs
+++ Assets/HomingEnemyScript.cs
@@ -3,30 +3,30 @@ using UnityEngine;
 
 public class HomingEnemyScript : MonoBehaviour {
     [SerializeField]
-    Transform player;
+    GameObject player;
     [SerializeField]
     float step;
-    Transform temporary;
-    float playerX;
-    float playerY;
-    float playerZ;
+    [SerializeField]
+    float moveDelay;
+    GameObject tempPlayer;
 
-    IEnumerator DelayTime()
-    {
-        yield return new WaitForSeconds(0);
-        playerX = player.position.x;
-        playerY = player.position.y;
-        playerZ = player.position.z;
-        temporary.transform.position = new Vector3(playerX, playerY, playerZ);
-    }
 
     // Use this for initialization
-    void Start () {
-        StartCoroutine(DelayTime());
+    void Awake () {
+        tempPlayer = new GameObject("player");
+        tempPlayer.transform.position = player.transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector3.MoveTowards(transform.position, temporary.position, 3);
+        StartCoroutine(DelayTime());
 	}
+
+    private IEnumerator DelayTime()
+    {
+        yield return new WaitForSeconds(moveSpeed);
+        transform.position = Vector3.MoveTowards(transform.position, tempPlayer.transform.position, 3);  //This is the movement to be changed
+
+    }
+
 }
