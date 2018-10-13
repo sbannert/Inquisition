bcd293e10c61f7cf1f077c91323a4c6e0c109e82
Robert Osonma <35873421+master248@users.noreply.github.com>
2018-10-13 14:37:23 -0500

    added a red koopa style enemy

---
+++ Assets/SmartMoveEnemyScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SmartMoveEnemyScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField]
+    float speed;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //speed = 2;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 vect = new Vector3(speed, 0, 0);
+        rb.MovePosition(transform.position + vect * Time.deltaTime);
+        /*if (rb.position.x < 19) {
+            speed = -2;
+        }*/
+        /*if (rb.position.x > 13)
+        {
+            speed = 2;
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.position.x > 19.0f)
+        {
+            speed = speed * -1;
+        }
+        if (rb.position.x < 13.0f)
+        {
+            speed = speed * -1;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
