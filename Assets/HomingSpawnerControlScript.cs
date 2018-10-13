d0edbd96f3bd9eddb6dfbc26011e9c1142a582f0
geirgie25 <43558674+geirgie25@users.noreply.github.com>
2018-10-13 18:12:36 -0500

    first level

---
+++ Assets/HomingSpawnerControlScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingSpawnerControlScript : MonoBehaviour {
+
+    [SerializeField]
+    float spawnDelay;
+    [SerializeField]
+    GameObject homing;
+    Vector3 vectLocation;
+    bool hasSpawned;
+
+    void Start()
+    {
+        vectLocation = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        hasSpawned = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.gameObject.name == "Player") && (!hasSpawned))
+        {
+            GameObject homingObj = (GameObject)Instantiate(homing, vectLocation, transform.rotation);
+            //spikeObj.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            homingObj.transform.parent = transform;
+            hasSpawned = true;
+        }
+    }
+}
