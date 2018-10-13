401456a1c9e6af7b7d8c3369c96b9cc927ad421c
geirgie25 <43558674+geirgie25@users.noreply.github.com>
2018-10-13 12:47:13 -0500

    fix plat

--- Assets/Scripts/Enemies/PlatformHeadScript.cs
+++ Assets/Scripts/Enemies/PlatformHeadScript.cs
@@ -27,7 +27,7 @@ public class PlatformHeadScript : MonoBehaviour {
         for (int i = 0; i < 200; i++)
         {
             yield return new WaitForSeconds(shrinkInterval);
-            transform.localScale -= new Vector3(0, 0.01f, 0);
+            //transform.localScale -= new Vector3(0, 0.01f, 0);
             transform.localPosition -= new Vector3(0, 0.01f, 0);
         }
     }
