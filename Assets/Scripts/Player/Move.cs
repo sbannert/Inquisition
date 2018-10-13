f533e53681c3e6d9140e7a6fcaa5fc9146688c0e
sbannert <sbannert@gmail.com>
2018-10-13 01:25:43 -0500

    Added movement and jump (#1)

---
+++ Assets/Scripts/Player/Move.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    [SerializeField]
+    float speed = 10f;
+    float go = 1f;
+
+    // Update is called once per frame
+    void Update ()
+    {
+            go = Time.deltaTime * speed;
+    }
+
+    public void Run(float moveX)
+    {
+        Vector3 movingVector = new Vector3(moveX, 0.0f, 0.0f);
+        movingVector *= go;
+
+        transform.Translate(movingVector, Space.World);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+    Move moveComponent;
+    //Jump jumpComponent;
+	// Use this for initialization
+	void Start ()
+    {
+        moveComponent = this.GetComponent<Move>();
+        //jumpComponent = this.GetComponent<Jump>();
+        if(moveComponent == null)
+        {
+            Debug.LogError("No Move instance found attatched to this game object.");
+        }
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float moveX = Input.GetAxis("MovementX");
+        //float moveY = Input.GetButton("Jump");
+
+        moveComponent.Run(moveX);
+        //jumpComponent.Start(moveY);
+
+	}
+}
